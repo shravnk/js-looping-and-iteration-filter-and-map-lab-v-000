2499de004db7b2ef4af5c100bcf74d2599d7fe99
Brendan O'Dea <brendan.ode4@gmail.com>
2018-05-15 19:47:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,6 +16,6 @@ function exactMatch(drivers, obj) {
   key = Object.keys(obj)[0]
   value = Object.keys(obj)[0]
   return drivers.filter(function(driver) {
-    return driver['key'] === value
+    return driver[key] === value
   })
 }
