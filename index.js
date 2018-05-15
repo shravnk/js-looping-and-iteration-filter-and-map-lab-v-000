f24e24c6e0aae38fb9aa1fd7b7f690a43bbbbff7
Brendan O'Dea <brendan.ode4@gmail.com>
2018-05-15 19:18:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,3 +5,10 @@ function driversWithRevenueOver(drivers, revenue) {
   }
 )
 }
+
+function driverNamesWithRevenueOver(drivers, revenue) {
+  return drivers.filter(function (driver) {
+    return driver.revenue > revenue
+  }
+)
+}
